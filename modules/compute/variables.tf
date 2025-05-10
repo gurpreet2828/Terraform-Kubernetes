fdc8077897afc2128b7d9033ad7287d4a539d462
Gurpreet <gurp28@my.yorku.ca>
2025-05-10 13:51:12 -0400

    terraform,yml codes

---
+++ modules/compute/variables.tf
@@ -0,0 +1,26 @@
+#-----compute/variables.tf-----
+#===============================
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ssh_key_public" {
+  type    = string
+  #Replace this with the location of you public key .pub
+  default = "~/.ssh/docker.pub"
+}
+
+variable "ssh_key_private" {
+  type    = string
+  #Replace this with the location of you private key
+  default = "~/.ssh/docker"
+}
+
+variable "public_subnet_one" {}
+variable "public_subnet_two" {}
+
+variable "security_group" {}
+
+variable "subnet_ip_one" {}
+variable "subnet_ip_two" {}
